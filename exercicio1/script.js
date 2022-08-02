d2e739c573e435a015213be0eb74feb750739989
Daniel <daniel.sprcp@gmail.com>
2022-08-02 16:03:05 -0300

    Exercício 1 finalizado

---
+++ exercicio1/script.js
@@ -0,0 +1,33 @@
+// EXERCÍCO 1:
+
+// CRIANDO ARRAY SACOLÃO:
+
+const sacolao = []
+
+// ITEM A: CRIANDO OBJETOS:
+
+const produto1 = {
+    nome: "banana",
+    preço: 1.00,
+    disponibilidade: true
+}
+
+const produto2 = {
+    nome: "alface",
+    preço: 2.00,
+    disponibilidade: true
+}
+
+const produto3 = {
+    nome: "tomate",
+    preço: 2.50,
+    disponibilidade: false
+}
+
+// ITEM B: ADICIONANDO OBJETOS AO ARRAY:
+
+sacolao.push(produto1, produto2, produto3)
+
+// ITEM C: IMPRIMINDO ARRAY NO CONSOLE:
+
+console.log(sacolao)
